5f7126595c4ef41398b7cff3efe0c11a2d2d1be8
vhosur <hvadiraj@hotmail.com>
2015-02-18 14:03:01 -0800

    dont need

--- gentn.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from sys import argv
-
-prog_name, fname =argv
-tnail=fname[:len(fname)-4]+'_tn.jpg'
-subprocess.call("ffmpeg -itsoffset -8 -i "+fname+" -vcodec mjpeg -vframes 1 -an -f rawvideo -s 120x90 -y "
-                                +tnail, shell=True)
--- mypython/settings.py
+++
@@ -1,6 +0,0 @@
-DATABASE = {'drivername':'postgres',
-      	    'host': 'localhost',
-            'port': '5432',
-            'username': 'vhosur',
-	    'password': 'test123',
-            'database': 'trial'}
